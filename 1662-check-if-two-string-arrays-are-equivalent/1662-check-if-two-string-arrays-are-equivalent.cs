b9b63b85ed347d6774c1ea94e64d0b94788984c5
Shakib Sarar Arnab <70606888+ssarnab@users.noreply.github.com>
2023-12-09 17:30:42 +0600

    Time: 96 ms (6.34%), Space: 40.5 MB (94.60%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cs
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cs
@@ -31,17 +31,48 @@ public class Solution {
         
         
         
-        StringBuilder one = new StringBuilder();
-        StringBuilder two = new StringBuilder();
+//         StringBuilder one = new StringBuilder();
+//         StringBuilder two = new StringBuilder();
 
-        foreach (string word in word1) {
-            one.Append(word);
+//         foreach (string word in word1) {
+//             one.Append(word);
+//         }
+
+//         foreach (string word in word2) {
+//             two.Append(word);
+//         }
+
+//         return one.ToString() == two.ToString();
+        
+              int i = 0; // index for word1
+        int j = 0; // index for word2
+        int charIndex1 = 0; // index for characters in word1[i]
+        int charIndex2 = 0; // index for characters in word2[j]
+
+        while (i < word1.Length && j < word2.Length) {
+            // Compare characters
+            if (word1[i][charIndex1] != word2[j][charIndex2]) {
+                return false;
+            }
+
+            // Move to the next character
+            charIndex1++;
+            charIndex2++;
+
+            // Move to the next word if we reach the end of a word
+            if (charIndex1 == word1[i].Length) {
+                i++;
+                charIndex1 = 0;
+            }
+
+            if (charIndex2 == word2[j].Length) {
+                j++;
+                charIndex2 = 0;
+            }
         }
 
-        foreach (string word in word2) {
-            two.Append(word);
-        }
-
-        return one.ToString() == two.ToString();
+        // Check if both arrays are exhausted
+        return i == word1.Length && j == word2.Length;
+        
     }
 }
