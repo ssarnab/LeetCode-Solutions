b58f8c02161da839f52a8cfa0c7ed51d397c306a
Shakib Sarar Arnab <70606888+ssarnab@users.noreply.github.com>
2024-04-07 14:50:09 +0600

    Time: 68 ms (56.36%) | Memory: 46.9 MB (63.40%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cs
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cs
@@ -1,49 +1,37 @@
 public class Solution {
-    public string MinRemoveToMakeValid(string s) {
-        int o = 0, cl = 0, f = 0;
-        foreach (char c in s) 
-        {
-            if (c == '(')
-            {
-                o++;
-                f++;
-            }
-            else if (c == ')' && f > 0)
-            {
-                cl++;
-                f--;
-            }
-        }
-        
-        int k = Math.Min(o, cl);
-        string re = "";
-        o = k;
-        cl = k;
-        foreach (char c in s)
-        {
-            if (c == '(')
-            {
-                if (o > 0)
-                {
-                    re += '(';
-                    o--;
-                }
-                continue;
-            }
-            if (c == ')')
-            {
-                if (cl > 0 && cl > o)
-                {
-                    re += ')';
-                    cl--;
-                }
-                continue;
-            } 
-            else
-            {
-                re += c;
-            }
-        }
-        return re;
-    }
+     public string MinRemoveToMakeValid(string s)
+ {
+     var stack = new Stack<int>(); // To keep track of '(' indices
+     var removeSet = new HashSet<int>(); // To mark indices for removal
+
+     // Identify invalid parentheses
+     for (int i = 0; i < s.Length; i++)
+     {
+         if (s[i] == '(')
+         {
+             stack.Push(i);
+         }
+         else if (s[i] == ')')
+         {
+             if (stack.Count > 0) stack.Pop(); // Found a matching pair
+             else removeSet.Add(i); // Unmatched ')' found, mark for removal
+         }
+     }
+
+     // Any remaining '(' in the stack are unmatched, mark them for removal
+     while (stack.Count > 0)
+     {
+         removeSet.Add(stack.Pop());
+     }
+
+     // Construct the valid string
+     var sb = new StringBuilder();
+
+     for (int i = 0; i < s.Length; i++)
+     {
+         if (!removeSet.Contains(i)) sb.Append(s[i]);
+     }
+
+     return sb.ToString();
+ }
 }
